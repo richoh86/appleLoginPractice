2ea0856b93f3db1ed64208b1487f2dc108b7ca91
richoh86 <wsoh1986@gmail.com>
2019-12-15 19:50:32 +0900

    Initial Commit

---
+++ appleLoginPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appleLoginPractice
+//
+//  Created by richoh86 on 2019/12/15.
+//  Copyright © 2019 richoh86. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
